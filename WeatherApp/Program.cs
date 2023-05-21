05c954ce2c90a9dd8abae0a416b441fcf4ec0a61
MrDMen 228 <lorrenc7@gmail.com>
2023-05-21 21:35:01 +0300

    Добавьте файлы проекта.

---
+++ WeatherApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace WeatherApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter city: ");
+            string city = Console.ReadLine();
+
+            WebRequest webRequest = WebRequest.Create($"http://api.openweathermap.org/data/2.5/weather?q={city}&appid=yourToken&units=metric");
+            webRequest.ContentType = "text/json";
+            webRequest.Method = "GET";
+
+            try
+            {
+                WebResponse response = webRequest.GetResponse(); ;
+
+
+                using (StreamReader sr = new StreamReader(response.GetResponseStream()))
+                {
+                    var result = sr.ReadToEnd();
+
+
+                    RootObject weather = JsonConvert.DeserializeObject<RootObject>(result);
+                    weather.PrintWeather();
+                }
+            }
+            catch(WebException we)
+            {
+                Console.WriteLine(we.Message);
+            }
+
+
+        }
+    }
+}
---
+++ WeatherApp/Weather.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp
+{
+    class Weather
+    {
+        public int Id { get; set; }
+        public string Main { get; set; }
+        public string Description { get; set; }
+        public string Icon { get; set; }
+    }
+
+    class Main
+    {
+        public string Temp { get; set; }
+        public string Feels_Like { get; set; }
+        public string Temp_Min { get; set; }
+        public string Temp_Max { get; set; }
+        public string Pressure { get; set; }
+        public string Humidity { get; set; }
+    }
+    class RootObject
+    {
+        public List<Weather> Weather { get; set; }
+        public Main Main { get; set; }
+
+
+        public string Name { get; set; }
+
+        public void PrintWeather()
+        {
+            Console.WriteLine($"**************Weather for: {Name}**************");
+            Console.WriteLine($"Temperature: {Main.Temp} °C, feels like {Main.Feels_Like} °C");
+            Console.WriteLine($"Max temp: {Main.Temp_Max} °C; Min temp: {Main.Temp_Min} °C");
+            Console.WriteLine($"Pressure: {Main.Pressure} hPa");
+            Console.WriteLine($"Air humidity: {Main.Humidity}%");
+            foreach(Weather w in Weather)
+            {
+                Console.WriteLine($"Weather: {w.Main}");
+                Console.WriteLine($"Weather description: {w.Description}");
+            }
+        }
+    }
+
+
+}
